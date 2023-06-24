7d8f7eaf53234497b8ce4e7c6787bfd026db92c6
OsvaldoLetran <oswaldo.letran@gmail.com>
2023-06-24 00:32:06 -0600

    file powerOfInteger added

---
+++ powerOfIntegers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int base, int n);
+
+int main()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("n = %d resultado de la potencia %d\n", i, power(2, i));
+    }
+    return 0;
+}
+
+int power(int base, int n)
+{
+    int i, p;
+    p = 1;
+    for (i = 0; i <= n; i++)
+    {
+        p = p*base;
+    }
+    return p;
+}
