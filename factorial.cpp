1d6cc2302c3f8b7f18c5296e072aebd190d1e318
OsvaldoLetran <oswaldo.letran@gmail.com>
2023-07-11 23:01:10 -0600

    factorial y fibo modified

--- factorial.cpp
+++ factorial.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
-int factorial(int n) {
-    if(n < 0) {
-				return 0;
-    } else if(n > 1) { 
-				return n * factorial(n - 1);
+int factorial(int n)
+{
+    if(n < 0)
+    {
+		return 0;
     } 
-		return 1;
+    else if(n > 1)
+    {
+		return n * factorial(n - 1);
+    } 
+	return 1;
 }
 
-int main(void) {
+int main(void)
+{
     int numero;
     cout << "ingresa un numero: ";
     cin >> numero;
--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,11 +6,11 @@ int fibonacci(int n)
 {
     if(n == 0 || n == 1)
     {
-	      return n;
+        return n;
     } else 
     {
-	      return fibonacci(n - 2) + fibonacci(n - 1);
-		}
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
 }
 
 int main()
@@ -21,19 +21,19 @@ int main()
     
     do
     {
-		    cout << "Ingrese un numero entero y positivo: ";
-		    cin >> num;
+        cout << "Ingrese un numero entero y positivo: ";
+        cin >> num;
     } while(num < 0);
     
-		cout << "\nLa serie es: \n\n";
+    cout << "\nLa serie es: \n\n";
     
     for(i = 0; i < num; i++)
     {
-	      if(fibonacci(i) != 0)
+        if(fibonacci(i) != 0)
         {
-			      cout << fibonacci(i);
+            cout << fibonacci(i);
             cout << ", ";
-		    }
+        }
     }
     return 0;
 }
