1579b087a8a6bad040a60a7eaa208584d61f068d
OsvaldoLetran <oswaldo.letran@gmail.com>
2023-07-11 23:06:07 -0600

    factorial modified

--- factorial.cpp
+++ factorial.cpp
@@ -6,13 +6,13 @@ int factorial(int n)
 {
     if(n < 0)
     {
-		return 0;
-    } 
+        return 0;
+    }
     else if(n > 1)
     {
-		return n * factorial(n - 1);
-    } 
-	return 1;
+        return n * factorial(n - 1);
+    }
+    return 1;
 }
 
 int main(void)
